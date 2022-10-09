d814f3886438d2a292587dd2217ae9b370a6a033
Dinal Kenula <dinalkenula7@gmail.com>
2022-10-09 13:34:24 +0530

    first commit

---
+++ homepage/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home_view(request):
+    return render(request, "home.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
+
+
+def historical(request):
+    return render(request, "historical.html")
+
+def exotic(request):
+    return render(request, "exoticp.html")
